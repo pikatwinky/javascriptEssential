2d7689dc80616c007d0cae13b6386f817136a96c
pikatwinky <pikatwinky.crimson@gmail.com>
2024-08-24 21:54:48 -0400

    grocery amount

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,11 +1,14 @@
-let length;
-let width;
+let amount1;
+let amount2;
+let amount3;
 
 function calculateArea() {
-length = parseFloat(document.getElementById('length').value);
-width = parseFloat(document.getElementById('width').value);
-let area = length * width;
+amount1 = parseFloat(document.getElementById('amount1').value);
+amount2 = parseFloat(document.getElementById('amount2').value);
+amount3 = parseFloat(document.getElementById('amount3').value);
 
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+let total = amount1+amount2+amount3;
+
+document.getElementById('result').innerText = `The area of the rectangle is: ${total}`;
 
 }
