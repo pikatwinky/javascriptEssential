b2a273bf82ef5824750a424920f20bbe2ae6a252
pikatwinky <pikatwinky.crimson@gmail.com>
2024-08-24 23:22:35 -0400

    product survey additional textarea

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,8 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
+    
     alert('Thank you for your valuable feedback')
 
     document.getElementById('userName').innerHTML = username;
@@ -15,6 +17,7 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
 
     document.getElementById('userInfo').style.display = 'block';
 }
